4e59c704bf01f4d86c12853f84fb250b996b6ab4
Diiegoorodrigo <diegorodrigosilva76@gmail.com>
2022-04-25 22:48:48 -0300

    att

---
+++ ArraysAvançado/0revisão.js
@@ -0,0 +1,6 @@
+// em arrays temos alguns comandos utilizados como
+// push para adicionar no final do array e unshift para adicionar no inicio
+//shift para retirar no inicio do array e pop para retirar no fim
+// "..." para adicionar o valor de um outro array em uma variavel, para que possamos alterar ela sem que altere o valor da variavel raiz
+// slice que fatia o array, ex: slice(0, -1) mostrará o primeiro elemento do array e retirará o ultimo valor do array e assim por diante
+// split(' ') ou split (',') ira seprar um array unico como por exemplo um nome = 'Maria Rosa Silva', usando o split ela será seprada em três elementos dentro da variavel nome, porque ira seprara os elementos que receberam espaço entre eles.
